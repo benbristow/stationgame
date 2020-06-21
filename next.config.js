8829864836a2785dd5203610fd6fb6f360ea7f3a
Ben Bristow <ben@bristow.me>
2020-06-21 23:03:53 +0100

    Azure static sites

--- next.config.js
+++ next.config.js
@@ -1,2 +1,10 @@
 const withSass = require('@zeit/next-sass');
-module.exports = withSass();
+
+module.exports = withSass({
+  exportTrailingSlash: true,
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' }
+    };
+  }
+});
